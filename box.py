0ce3cb4ecfc6a459b7e39f5c7ae9749cfb18e58e
Peter Kapteyn <kapteynpeter@gmail.com>
2021-11-07 17:08:25 -0500

    Initial commit

---
+++ box.py
@@ -0,0 +1,19 @@
+import curses
+
+screen = curses.initscr()
+
+try:
+    screen.border(0)
+    box1 = screen.subwin(20, 20, 5, 5)
+    box1.box()
+    box1.touchwin()
+    box1.refresh()
+    box1.getch()
+    box2 = screen.subwin(20, 20, 5, 25)
+    box2.box()
+    box2.touchwin()
+    box2.refresh()
+    screen.getch()
+
+finally:
+    curses.endwin()
---
+++ notes.md
@@ -0,0 +1,3 @@
+| Date | Progress Notes |
+| ----------- | ----------- |
+| Nov 7, 2021 | I can now enter 'ctl + n' to split the box into two equal sized boxes.  |
