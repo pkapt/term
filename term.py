ff75a1e8e4cc5303c37f3d7b7d5d64da6067ebba
Peter Kapteyn <kapteynpeter@gmail.com>
2021-11-15 20:14:00 -0500

    Added a windows context handler

--- term.py
+++ term.py
@@ -1,8 +1,17 @@
 import curses
+from collections import OrderedDict
 
 CTL_N = 14
 CTL_Q = 17
 CTL_W = 23
+CTL_UP = 480
+CTL_DOWN = 481
+CTL_LEFT = 443
+CTL_RIGHT = 444
+
+STATUS_BAR_OFFSET_Y = 1
+
+STATUS_BAR_STR = "EXIT -> ctl+q | NEW WIN -> ctl+n | DEL WIN -> ctl+w"
 
 class CursesString():
     '''
@@ -36,83 +45,95 @@ class CursesString():
 class Window():
     # TODO: add handling for port
     def __init__(self, stdscr, lines, cols, y, x, port=None):
-        self.window = stdscr.subwin(lines, cols, y, x)
+        self.stdscr = stdscr
+        self.win = stdscr.subwin(lines, cols, y, x)
+        self._refresh()
+    
+    def resize(self, lines, cols, y, x):
+        self.win = self.stdscr.subwin(lines, cols, y, x)
+        self._refresh()
+        return self
 
+    def _refresh(self):
+        self.win.box()
+        self.win.touchwin()
+        self.win.refresh()
 
 class ContextHandler():
     '''
     This class should be used as a singleton. Only need one of these bad boy to manage context
     '''
-    def __init__(self):
-        self._windows = []
 
-    def addWindow():
+    MAX_WINDOWS = 4
+
+    def __init__(self):
+        self._windows = {}
+    
+    def init_stdscr(self, stdscr):
+        self.stdscr = stdscr
+        self._height, self._width = self.stdscr.getmaxyx()
+        self._height -= STATUS_BAR_OFFSET_Y
+
+    def add_window(self):
+        if len(self._windows) <= self.MAX_WINDOWS:
+            self._windows[len(self._windows)+1] = Window(self.stdscr,0,0,0,0)
+            self._configure_window_sizes(len(self._windows))
+
+    def del_current_window():
         pass
 
-def configure_window(stdscr, windows_queue, n):
-    windows_queue.clear()
-    height, width = stdscr.getmaxyx()
-    height = height-1
+    def _configure_window_sizes(self, n):
+        def one():
+            self._windows[1] = self._windows[1].resize(self._height, self._width, 0, 0)
 
-    def one():
-        windows_queue.append(Window(stdscr, height, width, 0, 0))
+        def two():
+            win_width = int(self._width/2)
+            self._windows[1] = self._windows[1].resize(self._height, win_width, 0, 0)
+            self._windows[2] = self._windows[2].resize(self._height, win_width, 0, win_width)
+        
+        def three():
+            win_width = int(self._width/3)
+            self._windows[1] = self._windows[1].resize(self._height, win_width, 0, 0)
+            self._windows[2] = self._windows[2].resize(self._height, win_width, 0, win_width)
+            self._windows[3] = self._windows[3].resize(self._height, win_width, 0, win_width*2)
+        
+        def four():
+            win_width = int(self._width/2)
+            win_height = int(self._height/2)
+            self._windows[1] = self._windows[1].resize(win_height, win_width, 0, 0)
+            self._windows[2] = self._windows[2].resize(win_height, win_width, 0, win_width)
+            self._windows[3] = self._windows[3].resize(win_height, win_width, win_height, 0)
+            self._windows[4] = self._windows[4].resize(win_height, win_width, win_height, win_width)
 
-    def two():
-        win_width = int(width/2)
-        windows_queue.append(Window(stdscr, height, win_width, 0, 0))
-        windows_queue.append(Window(stdscr, height, win_width, 0, win_width))
-    
-    def three():
-        win_width = int(width/3)
-        windows_queue.append(Window(stdscr, height, win_width, 0, 0))
-        windows_queue.append(Window(stdscr, height, win_width, 0, win_width))
-        windows_queue.append(Window(stdscr, height, win_width, 0, win_width*2))
-    
-    def four():
-        win_width = int(width/2)
-        win_height = int(height/2)
-        windows_queue.append(Window(stdscr, win_height, win_width, 0, 0))
-        windows_queue.append(Window(stdscr, win_height, win_width, 0, win_width))
-        windows_queue.append(Window(stdscr, win_height, win_width, win_height, 0))
-        windows_queue.append(Window(stdscr, win_height, win_width, win_height, win_width))
+        win_handler_table = {
+            1 : one,
+            2 : two,
+            3 : three,
+            4 : four
+        }
 
-    def refresh_all():
-        for window in windows_queue:
-            window.window.box()
-            window.window.touchwin()
-            window.window.refresh()
+        handler_func = win_handler_table[n]
+        if callable(handler_func):
+            self.stdscr.clear()
+            handler_func()
 
-    win_handler_table = {
-        1 : one,
-        2 : two,
-        3 : three,
-        4 : four
-    }
-
-    handler_func = win_handler_table[n]
-    if callable(handler_func):
-        stdscr.clear()
-        handler_func()
-        refresh_all()
-
-def add_window(stdscr, windows_queue):
-    len_q = len(windows_queue)
-    if 1 <= len_q <= 3:
-        configure_window(stdscr, windows_queue, len_q + 1)
-
-def remove_window(stdscr, windows_queue):
-    len_q = len(windows_queue)
-    if 2 <= len_q <= 4:
-        configure_window(stdscr, windows_queue, len_q - 1)
+# make this big so everyone knows it's a singleton
+CONTEXT_HANDLER = ContextHandler()
 
 keypress_callback_table = {
-    CTL_N : add_window,
-    CTL_W : remove_window
+    CTL_N     : CONTEXT_HANDLER.add_window,
+    CTL_W     : None,
+    CTL_UP    : None,
+    CTL_DOWN  : None,
+    CTL_LEFT  : None,
+    CTL_RIGHT : None,
 }
 
-def handle_keypress(key, stdscr, windows_queue):
+def handle_keypress(key):
     try:
-        keypress_callback_table[key](stdscr, windows_queue)
+        handler_func = keypress_callback_table[key]
+        if callable(handler_func):
+            handler_func()
     except KeyError:
         pass
 
@@ -125,21 +146,19 @@ def init_colors():
 def draw_menu(stdscr):
     k = 0
 
+    CONTEXT_HANDLER.init_stdscr(stdscr)
+    CONTEXT_HANDLER.add_window()
+
     init_colors()
 
-    windows_queue = []
-    configure_window(stdscr, windows_queue, 1)
+    # windows_queue = []
+    # configure_window(stdscr, windows_queue, 1)
 
     # # Loop where k is the last character pressed
     while (k != CTL_Q):
-        handle_keypress(k, stdscr, windows_queue)
+        handle_keypress(k)
 
-        CursesString(
-            stdscr,
-            text = "EXIT -> ctl+q | NEW WIN -> ctl+n | DEL WIN -> ctl+w",
-            color = COLOR_BL_WH,
-            x = 0, y = -1, fill = -1
-        )
+        CursesString(stdscr, text = STATUS_BAR_STR, color = COLOR_BL_WH, x = 0, y = -1, fill = -1)
 
         k = stdscr.getch()
 
