e3ed10c7eb55b7867d8aea7c09d155a3468e9164
Peter Kapteyn <kapteynpeter@gmail.com>
2021-11-15 19:08:27 -0500

    Moved window to class so I can tie windows and their serial connections together

--- notes.md
+++ notes.md
@@ -2,4 +2,5 @@
 | ----------- | ----------- | ----------- |
 | Nov 7, 2021 | | I can now enter 'ctl + n' to split the box into two equal sized boxes.  |
 | Nov 9, 2021 |  | Can make & destroy up to 4 windows. Cleaned up the logic a bit. Next steps are to figure out header and footer banners & some UI stuff  |
-| Nov 14, 2021 | Add a system to add text on the top & bottom and maybe add text in each individual screen | |
+| Nov 14, 2021 | Add a system to add text on the top & bottom and maybe add text in each individual screen | Added a CursesString class, and a status string that shows commands on the bottom. Todo next time: get a unified way to adjust the box size based on the header/bottom strings|
+| Nov 15, 2021 | Add a context handler to focus on one window or another | |
--- term.py
+++ term.py
@@ -4,28 +4,50 @@ CTL_N = 14
 CTL_Q = 17
 CTL_W = 23
 
-
-COLOR_CY_BL = 1
-COLOR_BL_WH = 2
-
 class CursesString():
+    '''
+    @param stdscr curses obj
+    @param text string to display
+    @param x, y coordinates of string (can negative index)
+    @param fill fill the rest of the line with spaces
+    '''
     def __init__(self, stdscr, text, color, x, y, fill=None):
         self.text = text
         self.color = color
         self.x = x
         self.y = y
         self.fill = fill
-        height, width = stdscr.getmaxyx()
-        if y < 0:
-            y = height + y
-        if x < 0:
-            x = width + x
-        stdscr.attron(curses.color_pair(self.color))
-        stdscr.addstr(y, x, self.text)
-        if fill == -1:
-            stdscr.addstr(y, len(self.text), " " * (width - len(self.text) - 1))
-        stdscr.attroff(curses.color_pair(self.color))
-        stdscr.refresh()
+        self.stdscr = stdscr
+        self._render()
+    
+    def _render(self):
+        height, width = self.stdscr.getmaxyx()
+        if self.y < 0:
+            self.y = height + self.y
+        if self.x < 0:
+            self.x = width + self.x
+        self.stdscr.attron(curses.color_pair(self.color))
+        self.stdscr.addstr(self.y, self.x, self.text)
+        if self.fill == -1:
+            self.stdscr.addstr(self.y, len(self.text), " " * (width - len(self.text) - 1))
+        self.stdscr.attroff(curses.color_pair(self.color))
+        self.stdscr.refresh()
+
+class Window():
+    # TODO: add handling for port
+    def __init__(self, stdscr, lines, cols, y, x, port=None):
+        self.window = stdscr.subwin(lines, cols, y, x)
+
+
+class ContextHandler():
+    '''
+    This class should be used as a singleton. Only need one of these bad boy to manage context
+    '''
+    def __init__(self):
+        self._windows = []
+
+    def addWindow():
+        pass
 
 def configure_window(stdscr, windows_queue, n):
     windows_queue.clear()
@@ -33,32 +55,32 @@ def configure_window(stdscr, windows_queue, n):
     height = height-1
 
     def one():
-        windows_queue.append(stdscr.subwin(height, width, 0, 0))
+        windows_queue.append(Window(stdscr, height, width, 0, 0))
 
     def two():
         win_width = int(width/2)
-        windows_queue.append(stdscr.subwin(height, win_width, 0, 0))
-        windows_queue.append(stdscr.subwin(height, win_width, 0, win_width))
+        windows_queue.append(Window(stdscr, height, win_width, 0, 0))
+        windows_queue.append(Window(stdscr, height, win_width, 0, win_width))
     
     def three():
         win_width = int(width/3)
-        windows_queue.append(stdscr.subwin(height, win_width, 0, 0))
-        windows_queue.append(stdscr.subwin(height, win_width, 0, win_width))
-        windows_queue.append(stdscr.subwin(height, win_width, 0, win_width*2))
+        windows_queue.append(Window(stdscr, height, win_width, 0, 0))
+        windows_queue.append(Window(stdscr, height, win_width, 0, win_width))
+        windows_queue.append(Window(stdscr, height, win_width, 0, win_width*2))
     
     def four():
         win_width = int(width/2)
         win_height = int(height/2)
-        windows_queue.append(stdscr.subwin(win_height, win_width, 0, 0))
-        windows_queue.append(stdscr.subwin(win_height, win_width, 0, win_width))
-        windows_queue.append(stdscr.subwin(win_height, win_width, win_height, 0))
-        windows_queue.append(stdscr.subwin(win_height, win_width, win_height, win_width))
+        windows_queue.append(Window(stdscr, win_height, win_width, 0, 0))
+        windows_queue.append(Window(stdscr, win_height, win_width, 0, win_width))
+        windows_queue.append(Window(stdscr, win_height, win_width, win_height, 0))
+        windows_queue.append(Window(stdscr, win_height, win_width, win_height, win_width))
 
     def refresh_all():
         for window in windows_queue:
-            window.box()
-            window.touchwin()
-            window.refresh()
+            window.window.box()
+            window.window.touchwin()
+            window.window.refresh()
 
     win_handler_table = {
         1 : one,
@@ -94,6 +116,8 @@ def handle_keypress(key, stdscr, windows_queue):
     except KeyError:
         pass
 
+COLOR_CY_BL = 1
+COLOR_BL_WH = 2
 def init_colors():
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
