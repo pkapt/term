e55a61a4f477108faada9f5d0988f7fd4f405f8e
Peter Kapteyn <kapteynpeter@gmail.com>
2021-11-17 21:41:00 -0500

    Add remove functionality

--- term.py
+++ term.py
@@ -63,7 +63,6 @@ class Window():
     
     def resize(self, lines, cols, y, x):
         self.win = self.stdscr.subwin(lines, cols, y, x)
-        self.refresh()
         return self
         
     # TODO clean up string show logic somehow instead of just a rogue if statement
@@ -86,7 +85,7 @@ class WindowsManager():
     MAX_WINDOWS = 4
 
     def __init__(self, win_id_vis=False):
-        self._windows = {}
+        self._windows = OrderedDict()
         self._active_window = 1
         self._win_id_vis = win_id_vis
     
@@ -105,8 +104,15 @@ class WindowsManager():
             self._windows[window_id] = Window(self.stdscr,window_id,0,0,0,0,win_id_vis=self._win_id_vis)
             self._configure_windows(len(self._windows))
 
-    def del_current_window():
-        pass
+        # window.scrollok(flag)
+        # Control what happens when the cursor of a window is moved off the edge of the window or scrolling region, either as a result of a newline action on the bottom line, or typing the last character of the last line. If flag is False, the cursor is left on the bottom line. If flag is True, the window is scrolled up one line. Note that in order to get the physical scrolling effect on the terminal, it is also necessary to call idlok().    
+
+    def del_active_window(self):
+        if len(self._windows) > 1:
+            if self._active_window > 1 and self._active_window == len(self._windows):
+                self._active_window -= 1
+            self._windows.popitem()
+            self._configure_windows(len(self._windows))
 
     def _refresh_all(self):
         for window in self._windows:
@@ -182,7 +188,7 @@ class WindowsManager():
                 if self._active_window < 3:
                     return self._set_active_window(self._active_window + 1)
             elif dir == self.Directions.LEFT:
-                if self._active_window > 0:
+                if self._active_window > 1:
                     return self._set_active_window(self._active_window - 1)
 
         def handle_four(self, dir):
@@ -235,7 +241,7 @@ WIN_MANAGER = WindowsManager(win_id_vis=True)
 
 keypress_callback_table = {
     CTL_N     : WIN_MANAGER.add_window,
-    CTL_W     : None,
+    CTL_W     : WIN_MANAGER.del_active_window,
     CTL_UP    : WIN_MANAGER.give_context_up,
     CTL_DOWN  : WIN_MANAGER.give_context_down,
     CTL_LEFT  : WIN_MANAGER.give_context_left,
@@ -261,9 +267,11 @@ def draw_menu(stdscr):
 
     # # Loop where k is the last character pressed
     while (k != CTL_Q):
-        handle_keypress(k)
+        handle_keypress(k)  
 
         CursesString(stdscr, text = STATUS_BAR_STR, color = COLOR_BL_WH, x = 0, y = -1, fill = -1)
+
+        curses.beep()
 
         k = stdscr.getch()
 
